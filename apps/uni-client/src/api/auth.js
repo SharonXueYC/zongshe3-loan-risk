836b49df05f59a8c5ff1b86702bb7e993f567b3c
eleyoufanma <2425997939@qq.com>
2026-09-11 15:37:36 +0800

    Implement login and register functions in auth.js

--- apps/uni-client/src/api/auth.js
+++ apps/uni-client/src/api/auth.js
@@ -1,1 +1,54 @@
+import request, { setToken } from '@/utils/request.js'
 
+function getData(res) {
+  return res?.data || res
+}
+
+function getToken(res) {
+  const data = getData(res)
+
+  return (
+    data?.token ||
+    data?.accessToken ||
+    data?.access_token ||
+    res?.token ||
+    res?.accessToken ||
+    ''
+  )
+}
+
+export async function login(phoneNumber, password) {
+  const res = await request({
+    url: '/api/users/login',
+    method: 'POST',
+    data: {
+      phoneNumber,
+      password
+    },
+    auth: false
+  })
+
+  const token = getToken(res)
+
+  if (!token) {
+    throw new Error('后端未返回 token')
+  }
+
+  setToken(token)
+
+  return res
+}
+
+export function register(data) {
+  return request({
+    url: '/api/users/register',
+    method: 'POST',
+    auth: false,
+    data: {
+      phoneNumber: data.phoneNumber,
+      password: data.password,
+      userName: data.userName,
+      channelId: data.channelId || null
+    }
+  })
+}
